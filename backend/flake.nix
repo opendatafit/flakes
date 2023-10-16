cf34f1b40ea3c8106699e8acde93d74e19d5c93f
James Wilmot <jameswilmot2000@gmail.com>
2023-10-16 18:50:23 +1100

    backend flake: added python-mimeparse

--- backend/flake.nix
+++ backend/flake.nix
@@ -20,7 +20,7 @@
           # Set the major version of Node.js
           pkgs.python310
           pkgs.python310.pkgs.pip
-          pkgs.python310.pkgs.mimeparse
+          pkgs.python310.pkgs.python-mimeparse
         ];
       };
     });
